a3e02783df7b9f50360aaf6062d4e1ed28e8a4ee
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2024-11-19 16:12:41 +0700

    Update getAirQuality.tsx

--- src/libs/otherBackend/getAirQuality.tsx
+++ src/libs/otherBackend/getAirQuality.tsx
@@ -1,7 +1,7 @@
 import { ReceiveAirQuality } from "../../../interface";
 
 export default async function getAirQuality():Promise<ReceiveAirQuality>{
-    const res=await fetch('https://website-api.airvisual.com/v1/stations/s7ygLWXNth22qrdZH/measurements?units.temperature=celsius&units.distance=kilometer&units.pressure=millibar&units.system=metric&AQI=US&language=en',{
+    const res=await fetch('https://website-api.airvisual.com/v1/stations/s7ygLWXNth22qrdZH/measurements?units.temperature=celsius&units',{
         cache:'no-store'
     })
     return await res.json()
